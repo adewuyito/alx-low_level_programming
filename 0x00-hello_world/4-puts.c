e2a7f5b6ee488f17315dc51197a0635f6994af7d
adewuyito <adewuyitimothy10@gmail.com>
2023-03-16 16:34:32 -0700

    Last edit on 4-file

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -11,7 +11,7 @@ int main(void)
 {
 	char str[80];
 
-	strcpy(str, "Programming is like building a multilingual puzzle\n");
+	strcpy(str, "\"Programming is like building a multilingual puzzle");
 	puts(str);
 
 	return (0);
