f6f0120ed6e3c5ad623781fafc39a329c4fdd233
adewuyito <adewuyitimothy10@gmail.com>
2023-03-16 17:17:31 -0700

    Added 6-file

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	printf("Size of char: %lu byte(s)\n", (unsigned long)sizeof(char));
+	printf("Size of int: %lu byte(s)\n", (unsigned long)sizeof(int));
+	printf("Size of long int: %lu byte(s)\n", (unsigned long)sizeof(int));
+	printf("Size of long long: %lu byte(s)\n", (unsigned long)sizeof(long long));
+	printf("Size of float: %lu byte(s)\n", (unsigned long)sizeof(float));
+
+	return (0);
+}
