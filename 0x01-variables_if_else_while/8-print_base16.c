8814181d3f171f4e44e44243dbf239e2df25960b
adewuyito <adewuyitimothy10@gmail.com>
2023-03-17 18:14:10 -0700

    Added 8-file

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <time.h>
+#include <string.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char hex_digits[] = "0123456789abcdef";
+	int i;
+
+	for (i = 0; i < 16; i++)
+	{
+	putchar(hex_digits[i]);
+	}
+	putchar('\n');
+
+	return (0);
+}
