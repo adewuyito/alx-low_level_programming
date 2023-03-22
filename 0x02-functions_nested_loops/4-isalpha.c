7f44ff78771722b130bf50b32dab383f277f2ab5
adewuyito <adewuyitimothy10@gmail.com>
2023-03-22 03:44:04 +0100

    Added 4-function

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isalpha - Check for alpha char
+ * @c: The char to be cheched
+ * Return: 1 for alpha or 0 on any other thing
+ */
+
+int _isalpha(int c)
+{
+	if((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
+	{
+		return (1);
+	}
+	return (0);
+}
