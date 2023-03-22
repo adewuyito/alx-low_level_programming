362d2ecf2bc6dbb9d2650bdf86d444e050776570
adewuyito <adewuyitimothy10@gmail.com>
2023-03-22 14:51:50 +0100

    Added 8-file

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * jack_bauer - Prints all the minuts in 24hours
+ *
+ * Return: Void
+ */
+
+void jack_bauer(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i < 24; ++i)
+	{
+		for (j = i; j < 60; ++j)
+		{
+			_putchar('0' + i / 10);
+			_putchar('0' + i % 10);
+			_putchar(58);
+			_putchar('0' + j / 10);
+			_putchar('0' + j % 10);
+			_putchar('\n');
+		}
+	}
+}
