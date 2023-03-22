6d8dbb3a9689289aa3038da2e08884a84234011d
adewuyito <adewuyitimothy10@gmail.com>
2023-03-22 06:29:01 +0100

    Added 7-fun

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_last_digit - Print the last digit of an int
+ * @n: The int to be checked
+ * Return: Always 0
+ */
+
+int print_last_digit(int n)
+{
+	int last;
+
+	if (n < 0)
+	{
+		int con;
+
+		con = n * -1;
+		last = con;
+	}
+	else
+	{
+		last = n;
+	}
+	last = last % 10;
+
+	return (last);
+}
