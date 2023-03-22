9e6bbf76d5db75cbe6c0fc5d9da50176fb64a884
adewuyito <adewuyitimothy10@gmail.com>
2023-03-22 03:33:26 +0100

    Added 3-file

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/** _islower - check for lower charaachters
+ *
+ * Return: 1 for lower char or 0 anything else
+ */
+
+int _islowe(int c)
+{
+	if (c >= 97 && c <= 122)
+	{
+		return (1);
+	}
+	return (0);
+}
