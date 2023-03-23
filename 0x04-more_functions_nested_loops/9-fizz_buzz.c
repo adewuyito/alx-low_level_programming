d93ffc51f205b1b876fa434fa3df22eac2e68413
adewuyito <adewuyitimothy10@gmail.com>
2023-03-23 23:14:51 +0100

    Edited -f 9-file

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -15,21 +15,22 @@ int main(void)
 	{
 		if (i % 3 == 0 && i % 5 == 0)
 		{
-			printf("FizzBuzz\n");
+			printf("FizzBuzz ");
 		}
 		else if (i % 5 == 0)
 		{
-			printf("Buzz\n");
+			printf("Buzz ");
 		}
 		else if (i % 3 == 0)
 		{
-			printf("Fizz\n");
+			printf("Fizz ");
 		}
 		else
 		{
-			printf("%d\n", i);
+			printf("%d ", i);
 		}
 	}
+	printf("\n");
 
 	return (0);
 }
