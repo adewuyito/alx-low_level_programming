5aa9024774dc61ffe88991b28fccb55b82c40b9e
Timothy Adewuyi <adewuyitimothy10@gmail.com>
2023-03-28 01:31:37 +0000

    Added 6 file

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+*puts2 - Pronts every other char in a string
+*@str: String to print
+*Return: Void
+*/
+
+
+void puts2(char *str)
+{
+int i;
+
+i = 0;
+while (str[i] != '\n')
+{
+if (i % 2 != 0)
+{
+i++;
+}
+else
+{
+_putchar(str[i]);
+i++;
+}
+} 
+}
