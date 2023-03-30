534a5fec94fdcd0447a2e7e2e8236550ef534246
adewuyito <adewuyitimothy10@gmail.com>
2023-03-30 10:21:38 +0100

    edited 1-file

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -17,7 +17,7 @@ char *_strncat(char *dest, char *src, int n)
 	{
 		count++;
 	}
-	for (i = 0; i < n src[i] != '\0'; i++)
+	for (i = 0; i < n && src[i] != '\0'; i++)
 	{
 		dest[count] = src[i];
 		count++;
