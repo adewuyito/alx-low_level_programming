178c2b2db10dce0ba1a363e8b64bab9ad8a9c06d
adewuyito <adewuyitimothy10@gmail.com>
2023-04-05 00:54:05 +0100

    Added 100-file and modified main-file

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * set_string - Coopies a string from one location to another
+ * @s: The memory location to set
+ * @to: The memory location to copy from
+ *
+ * Return: Void
+ */
+void set_string(char **s, char * to)
+{
+	*s = to;
+}
