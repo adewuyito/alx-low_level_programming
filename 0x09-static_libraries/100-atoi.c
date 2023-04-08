4310cdcae1c4f5caf21098ca973d4040542db441
adewuyito <adewuyitimothy10@gmail.com>
2023-04-08 15:54:44 +0100

    Edited 100-file

---
+++ 0x09-static_libraries/100-atoi.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+int _atoi(char *s)
+{
+	int sign;
+	int result, i;
+
+	sign = 1;
+	result = 0;
+	i = 0;
+	while (isspace(s[i]))
+	{
+		i++;
+	}
+	if (s[i] == '-' || s[i] == '+')
+	{
+		if (s[i] == '-')
+			sign = -1;
+		i++;
+	}
+
+	while (isdigit(s[i]))
+	{
+		result = result * 10 + (s[i] - '0');
+		i++;
+	}
+
+	return (result * sign);
+}
