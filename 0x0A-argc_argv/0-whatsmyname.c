643c2023e2701c262acc8bee12568ba8e5d60612
adewuyito <adewuyitimothy10@gmail.com>
2023-04-08 03:33:59 +0100

    Edited 0-file

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -12,16 +12,11 @@ int main(int argc, char *argv[])
 {
 	(void)argc;
 
-	int i, j;
+	int i;
 
 	for (i = 0; i < argc; i++)
 	{
-		for (j = 0; argv[i][j] != '\0'; j++)
-		{
-
-			_putchar(argv[i][j]);
-		}
-		_putchar('\n');
+		printf("%s\n", argv[i]);
 	}
 	exit(EXIT_SUCCESS);
 }
