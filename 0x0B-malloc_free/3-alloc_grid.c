aba7ad47af48c0f1131e0fd69b246bb86dc0a9dc
adewuyito <adewuyitimothy10@gmail.com>
2023-04-12 16:24:08 +0100

    Edited 3-file

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -13,6 +13,10 @@ int **alloc_grid(int width, int height)
 	int **array;
 	int i, j;
 
+	if (width <= 0 || height <= 0)
+	{
+		return (NULL);
+	}
 	array = (int **)malloc(height * sizeof(int *));
 
 	for (i = 0; i < height; i++)
