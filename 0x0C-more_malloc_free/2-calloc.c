07eb823798edb3533cdbb9b556abbd572ab5a2a2
adewuyito <adewuyitimothy10@gmail.com>
2023-04-13 21:45:37 +0100

    Added 2-file

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _calloc - Assigns memory like calloc
+ * @nmemb: Ammount of items
+ * @size: item size
+ *
+ * Return: Pointer to mem location
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	int *mem_location;
+	size_t i;
+
+	if (nmemb == 0 || size == 0)
+	{
+		/*printf("Im not sure what this is");*/
+		return (NULL);
+	}
+	mem_location = malloc(nmemb * sizeof(size));
+	if (mem_location == NULL)
+	{
+		/*printf("memory failed");*/
+		return (NULL);
+	}
+	for (i = 0; i <= nmemb; i++)
+	{
+		mem_location[i] = 0;
+	}
+
+	return (mem_location);
+}
