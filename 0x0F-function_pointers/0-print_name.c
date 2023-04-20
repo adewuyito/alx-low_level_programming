3b6195ed40af5baec78df14b32be5624cbeafeb3
adewuyito <adewuyitimothy10@gmail.com>
2023-04-20 13:10:39 +0100

    New 0-file

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,15 +1,17 @@
 #include "function_pointers.h"
 
 /**
-* print_name - prints a string
-* @name: the name to print
-* @f: the function to call
-*
-* Return: Void
-*/
+ * print_name - prints a string
+ * @name: the name to print
+ * @f: the function to call
+ *
+ * Return: Void
+ */
 void print_name(char *name, void (*f)(char *))
 {
 	if (name == NULL || f == NULL)
+	{
 		return;
+	}
 	f(name);
 }
