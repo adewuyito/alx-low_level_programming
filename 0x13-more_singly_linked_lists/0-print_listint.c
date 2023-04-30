a28856e1213ae4d976b0b73c6572faf1396e89da
adewuyito <adewuyitimothy10@gmail.com>
2023-04-30 21:18:00 +0100

    Edited These files

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -10,13 +10,9 @@ size_t print_listint(const listint_t *h)
 {
 	size_t count = 0;
 
-	if (h == NULL)
-	{
-		return (0);
-	}
 	while (h != NULL)
 	{
-		printf("%d \n", h->n);
+		printf("%u \n", h->n);
 		h = h->next;
 		count++;
 	}
--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -10,11 +10,7 @@ size_t listint_len(const listint_t *h)
 {
 	size_t count = 0;
 
-	if (h == NULL)
-	{
-		return (0);
-	}
-	while (h->next != NULL)
+	while (h != NULL)
 	{
 		h = h->next;
 		count++;
