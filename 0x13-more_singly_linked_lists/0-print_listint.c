0bda727d2d575e230eb72001d9fb37f777f5ae98
adewuyito <adewuyitimothy10@gmail.com>
2023-04-30 21:36:16 +0100

    Final edit 0-file

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -10,11 +10,10 @@ size_t print_listint(const listint_t *h)
 {
 	size_t count = 0;
 
-	while (h)
+	for (; h != NULL; h = h->next)
 	{
         count++;
 		printf("%u\n", h->n);
-		h = h->next;
 	}
 
 	return (count);
