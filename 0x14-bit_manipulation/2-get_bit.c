46d4c1fb09e6d4f250eac758e7b069008daee054
Timothy Adewuyi <adewuyitimothy10@gmail.com>
2023-09-01 11:22:51 +0100

    test commit

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -8,35 +8,5 @@
  */
 int get_bit(unsigned long int n, unsigned int find)
 {
-    int binary[16] = {0};
-    unsigned long int num = n;
-    int msb_found = 0;
-    int i;
-
-    if (find > 16 || num == NULL)
-    {
-        return (-1);
-    }
-
-    for (i = 16; i >= 0; i--)
-    {
-        int mask = (1 << i);
-
-        if (num & mask)
-        {
-            binary[i] = 1;
-            msb_found = 1;
-        }
-        else if (msb_found)
-        {
-            binary[i] = 0;
-        }
-        else if(num == 0)
-        {
-            binary[i] = 0;
-            break;
-        }
-    }
-
-    return (binary[find]);
+   return ((n >> find) & 1);
 }
