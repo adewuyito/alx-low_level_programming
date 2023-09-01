4ca5cec843c78d3cc6306cda88e73829c531cc3f
Timothy Adewuyi <adewuyitimothy10@gmail.com>
2023-09-01 11:47:03 +0100

    5-file test_fix

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -1,49 +1,29 @@
 #include "main.h"
 
 /**
- * flip_bits - number of biits to flip between two numbers
+ * flip_bits - number of bits to flip between two numbers
  * @n: The first in number
  * @m: The second in number
  * return: the number of flips
  */
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-	int msb_found1 = 0;
-	int msb_found2 = 0;
-	int binary1[16] = {0};
-	int binary2[16] = {0};
-	int running_amount = 0;
-	int i;
+	unsigned long int i = 0, j = 0, k = 0;
 
-	for (i = 16; i >= 0; i--)
-	{
-		int mask = (1 << i);
-
-		if (n & mask)
-		{
-			binary1[i] = 1;
-			msb_found1 = 1;
-		}
-		else if (msb_found1 || n == 0)
-		{
-			binary1[i] = 0;
-		}
-
-		if (m & mask)
-		{
-			binary2[i] = 1;
-			msb_found2 = 1;
-		}
-		else if (msb_found2 || m == 0)
-		{
-			binary2[i] = 0;
-		}
-
-		if (binary1[i] != binary2[i])
-		{
-			running_amount++;
-		}
-	}
-
-	return (running_amount);
+    while (n > 0 || m > 0)
+    {
+        if ((n & 1) != (m & 1))
+        {
+            i++;
+        }
+        n = n >> 1;
+        m = m >> 1;
+    }
+    while (i > 0)
+    {
+        j = i & 1;
+        k = k + j;
+        i = i >> 1;
+    }
+    return (k);
 }
