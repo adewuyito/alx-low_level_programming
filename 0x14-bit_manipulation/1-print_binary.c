c4f2a28bfff55c2bd3dc21daa7298a772537fb68
Timothy Adewuyi <adewuyitimothy10@gmail.com>
2023-09-01 10:57:13 +0100

    Added final files

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -11,25 +11,23 @@ void print_binary(unsigned long int n)
 	int msb_found = 0;
 	int i;
 
-	if (num != 0)
+	for (i = 16; i >= 0; i--)
 	{
-		for (i = 16; i >= 0; i--)
-		{
-			int mask = (1 << i);
+		int mask = (1 << i);
 
-			if (num & mask)
-			{
-				_putchar('1');
-				msb_found = 1;
-			}
-			else if (msb_found || num == 0)
-			{
-				_putchar('0');
-			}
+		if (num & mask)
+		{
+			_putchar('1');
+			msb_found = 1;
 		}
-	}
-	else
-	{
-		putchar('0');
+		else if (msb_found)
+		{
+			_putchar('0');
+		}
+		else if (num == 0)
+		{
+			_putchar('0');
+			break;
+		}
 	}
 }
