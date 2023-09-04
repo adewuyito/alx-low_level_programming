3c4788692a07a46bf20fbcfe9a30da42b75fce16
Timothy Adewuyi <adewuyitimothy10@gmail.com>
2023-09-04 23:00:37 +0100

    added cp-file

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -6,9 +6,6 @@
  * @letters: number of letters it should read and print
  *
  * Return: actual number of letters it could read and print, 0 if:
- * - the file can not be opened or read,
- * - filename is NULL,
- * - write fails or does not write the expected amount of bytes
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,61 @@
+#include "main.h"
+
+
+/**
+ * copy_file - copies the contents of one file to another
+ * @src: source file
+ * @dest: destination file
+ * Return: 1 on success, -1 on failure
+ */
+int main(int ac, char **av)
+{
+	FILE *src, *dest;
+	char *buf;
+	size_t length;
+
+	if (ac != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+
+	src = fopen(av[1], "r");
+	if (src == NULL)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
+		exit(98);
+	}
+
+	dest = fopen(av[2], "w+");
+	if (dest == NULL)
+	{
+		exit(99);
+	}
+
+	buf = malloc(sizeof(char) * 1024);
+	if (buf == NULL)
+	{
+		fclose(src);
+		fclose(dest);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
+		exit(99);
+	}
+
+	while ((length = fread(buf, sizeof(char), 1024, src)) > 0)
+	{
+		if (fwrite(buf, sizeof(char), length, dest) != length)
+		{
+			fclose(src);
+			fclose(dest);
+			free(buf);
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
+			exit(99);
+		}
+	}
+
+	free(buf);
+	fclose(src);
+	fclose(dest);
+
+	return (0);
+}
