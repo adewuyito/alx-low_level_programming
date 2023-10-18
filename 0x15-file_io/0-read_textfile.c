462eff83eb0fe482f6e726762becaa001eaac3fc
Timothy Adewuyi <adewuyitimothy10@gmail.com>
2023-10-19 00:33:45 +0100

    fixed spaces

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -7,7 +7,7 @@
  *
  * Return: actual number of letters it could read and print, 0 if:
  */
-ssize_t read_textfile(const char * filename, size_t letters)
+ssize_t read_textfile(const char *filename, size_t letters)
 {
 	FILE *fp;
 	char *buf;
@@ -17,22 +17,15 @@ ssize_t read_textfile(const char * filename, size_t letters)
 	size_t file_len = 0;
 
 	if (filename == NULL)
-	{
 		return (0);
-	}
 	fp = fopen(filename, "r");
 	if (fp == NULL)
-	{
 		return (0);
-	}
-
 	fseek(fp, 0, SEEK_END);
 	file_len = ftell(fp);
 	rewind(fp);
-
 	if (letters > file_len)
 		letters = file_len;
-
 	buf = malloc(sizeof(char) * letters);
 	if (buf == NULL)
 	{
@@ -47,10 +40,7 @@ ssize_t read_textfile(const char * filename, size_t letters)
 	}
 
 	for (i = 0; i < index; i++)
-	{
 		_putchar(buf[i]);
-	}
-
 	free(buf);
 	fclose(fp);
 
